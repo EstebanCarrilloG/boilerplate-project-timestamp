e75fc6b4e5be76aed06b8bb528f4cb32704e2541
Esteban Carrillo <estexav0996@gmail.com>
2024-11-17 20:13:27 +0000

    Timestamp Microservice done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +21,29 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
+});
+
+app.get("/api", function (req, res) {
+  const currentDate = new Date()
+  res.json({ unix: Date.parse(currentDate), utc: currentDate.toUTCString() })
+})
+
+app.get("/api/:date", function (req, res) {
+
+  const date_string = req.params.date;
+
+  let date = new Date(date_string)
+
+  if (date_string.match(/^(\d+)$/)) {
+    res.json({ unix: Number(date_string), utc: new Date(Number(date_string)).toUTCString() })
+  } else {
+    console.log(date)
+    if (date == "Invalid Date") {
+      return res.json({ error: "Invalid Date" })
+    }
+    res.json({ unix: Date.parse(date), utc: date.toUTCString() })
+  }
 });
 
 
